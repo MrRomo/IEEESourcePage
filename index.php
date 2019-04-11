f501da35c893b5ad0f014e6bbe2046ee33c6fd27
Ricardo Romo <jricardoromo21@gmail.com>
2019-04-11 16:18:38 -0500

    [FOLDER]

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>IEEE | Unimag</title>
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+<header>
+<div >
+
+<nav>
+
+<ul class="nav">
+	<li id="logo1"><img src="https://upload.wikimedia.org/wikipedia/en/thumb/2/21/IEEE_logo.svg/1200px-IEEE_logo.svg.png" width="60px" height="30px"></li>
+	<li class="menu"><a href="#">Registrate</a></li>
+	<li class="menu"><a href="#">| Iniciar session</a></li>
+	<li class="menu"><a href="#">Contactanos</a></li>   
+    <li class="menu"><a href="#">Noticias</a></li>
+    <li class="menu"><a href="#">Membresias</a></li>
+    
+    <li class="menu">
+        <a href="#">Eventos</a>
+        <ul>
+            <li ><a href="#" style="color:blue;">Cursos</a></li>
+            <li><a href="#" style="color:blue;">Conferencias</a></li>
+            <li><a href="#" style="color:blue;">Talleres</a></li><br>
+            <li><a href="#" style="color:blue;">Meetups</a></li>
+        </ul>
+    </li>
+        <li class="menu">
+        <a href="#">informacion</a>
+        <ul>
+        	<li><a href="#" style="color:blue;">Vision</a></li><br>
+            <li><a href="#" style="color:blue;">Mision</a></li><br>
+            <li><a href="#" style="color:blue;">Funciones</a></li>
+            <li><a href="#" style="color:blue;">Organigrama</a></li>
+        </ul>
+    </li>
+     <li class="menu"><a href="#">Inicio</a></li>
+<<<<<<< HEAD
+=======
+    
+    
+
+</ul>
+
+        </nav>	
+   </div>
+</header><!-- /header -->
+
+<body>
+	
+</body>
+</html>
+     <li id="menu"><a href="#">Inicio</a></li>
+>>>>>>> origin/master
+    
+    
+
+</ul>
+
+        </nav>	
+   </div>
+</header><!-- /header -->
+
+<body>
+	
+</body>
+</html>
